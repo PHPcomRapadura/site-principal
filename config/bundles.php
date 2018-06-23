2452c941d8ca557cadfbf8a9d9673b179d8a2585
Kilderson Sena <dersonsena@gmail.com>
2018-06-22 22:38:50 -0300

    Montagem do template gentelella na estrutura do SF4 com pagina do dashboard

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    App\AdminBundle\AdminBundle::class => ['all' => true]
 ];
