41b305428f49f65f9fe0b6843dcfded2e5ef4040
Kilderson Sena <dersonsena@gmail.com>
2018-06-29 14:13:22 -0300

    Revert "Concluido o sistema de login com autenticação. #issue_12"

--- config/bundles.php
+++ config/bundles.php
@@ -13,6 +13,5 @@ return [
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
-    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
-    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true]
 ];
