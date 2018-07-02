434d7cc0aa978e426070cadc59281d2e5782857a
Jholdy Damasceno <jholdydamasceno@gmail.com>
2018-07-01 21:11:37 -0300

    Correção tamanho do code para 25

--- src/Migrations/Version20180701212559.php
+++ src/Migrations/Version20180701212559.php
@@ -15,7 +15,7 @@ final class Version20180701212559 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE settings (id INT AUTO_INCREMENT NOT NULL, updated_by INT NOT NULL, code VARCHAR(10) NOT NULL, title VARCHAR(60) NOT NULL, description VARCHAR(100) DEFAULT NULL, value LONGTEXT NOT NULL, created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL, INDEX IDX_E545A0C516FE72E1 (updated_by), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE settings (id INT AUTO_INCREMENT NOT NULL, updated_by INT NOT NULL, code VARCHAR(25) NOT NULL, title VARCHAR(60) NOT NULL, description VARCHAR(100) DEFAULT NULL, value LONGTEXT NOT NULL, created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL, INDEX IDX_E545A0C516FE72E1 (updated_by), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('ALTER TABLE settings ADD CONSTRAINT FK_E545A0C516FE72E1 FOREIGN KEY (updated_by) REFERENCES users (id)');
     }
 
